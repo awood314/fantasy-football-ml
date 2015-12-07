2b2d5e8aa4f142456168337a0bcec5617143ae20
garrettjohnston <laker_magic32@sbcglobal.net>
2015-12-06 23:02:58 -0800

    Added complete player stats scraping, actually this time

--- data/scrapeProFootballReference.py
+++ data/scrapeProFootballReference.py
@@ -7,35 +7,67 @@ from pymongo import MongoClient
 
 def scrape_boxscore(boxscore_url, db):
     # Mongo collections
-    players = db['players']
+    playersClt = db['players']
 
-    # Game data
-    game_id = boxscore_url.split('boxscores/')[1]
-        
-    # Get the list of player names for each team
+    # Get team names
     page = requests.get(boxscore_url)
     tree = html.fromstring(page.content)
     teams = tree.xpath('//a[@href="#top"]/text()')[0].split(' @ ')
-    player_names = [] # away, home
-    player_names.append(tree.xpath('//h2[text()="' + teams[0] + ' Snap Counts"]/../..//td[@align="left"]/a/text()'))
-    player_names.append(tree.xpath('//h2[text()="' + teams[1] + ' Snap Counts"]/../..//td[@align="left"]/a/text()'))
-    
-    # Scrape player data for the game and add their game data to their collection
-    for i in range(0,2): 
-        for name in player_names[i]:
-            player_data = scrape_player(name, teams[i], tree)
-            players.update({'name':name, 'game':game_id}, player_data, upsert=True)
 
-def scrape_player(name, team, tree):
-    snapcount = tree.xpath('//div[div/h2/text()="' + team + ' Snap Counts"]//tr[td/a/text()="' + name + '"]/td')
-    pos = snapcount[1].text
-    return {'name': name, 
-            'pos' : pos }
-    
+    # Game data
+    game_data = {
+        "game_id" : boxscore_url.split('boxscores/')[1],
+        "away_team": teams[0],
+        "home_team": teams[1]
+        }
+
+
+    for i in range(0,2):
+        snapcount_row = tree.xpath('//div[div/h2/text()="' + teams[i] + ' Snap Counts"]/div/table/tbody/tr')
+        players = []
+        for row in snapcount_row:
+            cols = row.xpath('td')
+            name = cols[0].xpath('a')[0].text
+            position = cols[1].text
+            off_snapcount = cols[2].text
+            if int(off_snapcount) > 0:
+                player_stats = scrape_player_stats(name, tree)
+                player_stats.update(game_data)
+                player_stats.update({"snapcount": off_snapcount})
+                playersClt.update({"name": name, "position": position, "team": teams[i]}, {'$push': {'games': player_stats}}, True)
+
+# Game stats & data
+def scrape_player_stats(name, tree):
+    off_stats_row = tree.xpath('//table[@id="skill_stats"]//tr[td/a/text()="' + name + '"]/td')
+    off_stats = [0] * 17
+    for i in range(2,len(off_stats_row)):
+        if off_stats_row[i].text:
+            off_stats[i - 2] = int(off_stats_row[i].text)
+
+    return \
+        {
+            "pcmp": off_stats[0],
+            "patt": off_stats[1],
+            "pyds": off_stats[2],
+            "ptd":  off_stats[3],
+            "pint": off_stats[4],
+            "plng": off_stats[5],
+            "ratt": off_stats[6],
+            "rshyds": off_stats[7],
+            "rtd": off_stats[8],
+            "rlng": off_stats[9],
+            "rtgt": off_stats[10],
+            "rrec": off_stats[11],
+            "recyds": off_stats[12],
+            "rectd": off_stats[13],
+            "reclng": off_stats[14],
+            "fmb": off_stats[15],
+            "fmbl": off_stats[16]
+        }
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "usage: scrape.py <year>"
+        print("usage: scrape.py <year>")
     
     # Mongodb connection
     conn = MongoClient()
@@ -50,5 +82,5 @@ if __name__ == "__main__":
 
     # Scrape data for each game
     for boxscore_url in boxscore_urls:
+        print('scraping game', boxscore_url)
         scrape_boxscore(site + boxscore_url, db)
-
