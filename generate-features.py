509b168b4315d054e6ae1267d12dd058156d1b3f
alex <awood314@gmail.com>
2015-12-08 00:50:49 -0800

    stuff

--- generate-features.py
+++ generate-features.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3.4
-# Generates files with feature vectors for a specific position
+# Generates files with feature featuretors for a specific position
 import sys
 import operator
 from pymongo import MongoClient
 
 def generate_qb_features(player, db):
+    # Player collection
+    playerClt = db['players']
+
     # returns an array of strings to write as feature vectors
     features = []
 
     # Compute statistics at the time of each game on a cumulative/rolling basis
-    game_count = 0
-    for game in sorted(player['games'], key=operator.itemgetter('year','week')):
-        vec = ""
-        vec += str(game_count) + " "
+    games = sorted(player['games'], key=operator.itemgetter('year','week'))
+    avg_pyds = games[0]['pyds']
+    avg_rshyds = games[0]['rshyds']
+    avg_pass_pct = games[0]['pcmp'] / games[0]['patt']
+    game_count = 1
+    for game in games[1:]:
+        feature = ""
 
-        # average passing yards
-        avg_pyds = game['pyds'] if game_count == 0 else (avg_pyds + game['pyds']) / 2
-        vec += str(avg_pyds) + " "
-
-        # compute fantasy score label
-        # TODO - help me lol
-        score = int(game['pyds']/25) + game['ptd']*4 - game['pint']*2 + int(game['rshyds']/10) + game['rtd']*6 
-        vec += str(score)
-        
-        features.append(vec)
+        # Total number of games played
+        feature += str(game_count) + " "
         game_count += 1        
 
+        # average passing yards
+        avg_pyds = (avg_pyds + game['pyds']) / 2.
+        feature += str(avg_pyds) + " "
+        
+        # average rushing yards
+        avg_rshyds = (avg_rshyds + game['rshyds']) / 2.
+        feature += str(avg_rshyds) + " "
+
+        # average pass percentage
+        avg_pass_pct = (avg_pass_pct + (game['pcmp'] + game['patt'])) / 2
+        feature += str(avg_pass_pct) + " "
+
+        # Label: fantasy score
+        # TODO - help plz
+        score = int(game['pyds']/25) + game['ptd']*4 - game['pint']*2 + int(game['rshyds']/10) + game['rtd']*6 
+        feature += str(score)
+        features.append(feature)
     return features
 
 
@@ -40,7 +55,6 @@ if __name__ == "__main__":
 
     for player in playerClt.find():
         if player['position'] == 'QB':
-            print(player['name'])
             features = generate_qb_features(player, db)
             for feature in features:
                 qb_file.write(feature + "\n")
